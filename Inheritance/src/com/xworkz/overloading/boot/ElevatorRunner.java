316a9029590455f7418fc6cfe56ab19dd1b103cf
Archananaik2002 <archana.xworkz@gmail.com>
2023-08-07 21:58:02 +0530

    Method Overloading

---
+++ Inheritance/src/com/xworkz/overloading/boot/ElevatorRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.overloading.boot;
+
+import com.xworkz.overloading.app.Elevator;
+
+public class ElevatorRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Elevator elevator=new Elevator();
+		elevator.lift();
+
+	}
+
+}
