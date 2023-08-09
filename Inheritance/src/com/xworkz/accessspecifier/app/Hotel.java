e85f54ed8217a54787f5bcfde2df7ccca60efbe5
Archananaik2002 <archana.xworkz@gmail.com>
2023-08-09 21:43:26 +0530

    Method overriding

---
+++ Inheritance/src/com/xworkz/accessspecifier/app/Hotel.java
@@ -0,0 +1,28 @@
+package com.xworkz.accessspecifier.app;
+
+public class Hotel {
+	public void serve() {
+		System.out.println("Invoking serve in Hotel");
+	}
+	
+	public void serve(String name) {
+		System.out.println("Invoking serve with name in Hotel");
+	}
+	
+	public void serve(String name,String location) {
+		System.out.println("Invoking serve with name and location in Hotel");
+	}
+	
+	public void serve(String name,String location,String hotelType) {
+		System.out.println("Invoking serve with name, location and hotelType in Hotel");
+	}
+	
+	public void serve(int noOfCooks) {
+		System.out.println("Invoking serve with noOfCooks in Hotel");
+	}
+	
+	public void serve(String hotelType,int noOfCooks) {
+		System.out.println("Invoking serve with hotelType and noOfCooks in Hotel");
+	}
+
+}
